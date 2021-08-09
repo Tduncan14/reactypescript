7f75e431135ecfe2723b48bee3a99a50b30b2af9
tduncan14 <nbatre1@yahoo.com>
2021-08-09 16:54:29 -0700

    keep moving forward

---
+++ typescript/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+
+
+let name:string;
+let age:number;
+let isStudent:boolean;
+let hobbies: string[];
+let role:[number,string];
+
+
+// a way to typescript an object
+
+function App() {
+  return (
+    <div>
+   
+    </div>
+  );
+}
+
+export default App;
