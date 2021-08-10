9b13e3b3e462b691fb325411bc25e4881cd1e4d5
tduncan14 <nbatre1@yahoo.com>
2021-08-10 13:35:02 -0700

    keep moving forward

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import './App.css';
+import InputField from './components/InputField'
 
 
 
 let name:string;
-let age:number;
+let age:number | string;
 let isStudent:boolean;
 let hobbies: string[];
 let role:[number,string];
 
 
+
+interface person {
+  name:string,
+  age?: string | number
+}
+
 // a way to typescript an object
 
-function App() {
+const App: React.FC = ()  =>{
+
+  const [todo,setTodo] = useState<string>("")
   return (
-    <div>
+    <div className="App">
+
+      <span className="heading">Taskify</span>
+      <InputField/>
    
     </div>
   );
