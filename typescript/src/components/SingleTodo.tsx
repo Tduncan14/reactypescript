56aaf3f843da2faac2a61919f9d8320fe42e885c
tduncan14 <nbatre1@yahoo.com>
2021-08-11 16:08:42 -0700

    keep moving forward

--- typescript/src/components/SingleTodo.tsx
+++ typescript/src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Todo} from './model';
 import {AiFillEdit,AiFillDelete} from 'react-icons/ai'
 import {MdDone} from 'react-icons/md';
@@ -19,6 +19,49 @@ import './styles.css';
 const SingleTodo  = ({todo,todos,setTodos} :props) => {
 
 
+    const [edit,setEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>(todo.todo)
+
+
+    const handleDelete = (id:number) => {
+
+        setTodos(todos.filter((todo) => todo.id === id ? {...todo, todo:editTodo} : todo))
+
+
+      
+    }
+
+
+    const handleDone = (id:number) => {
+
+        setTodos(
+            todos.map((todo) =>
+            todo.id === id ? {...todo, isDone: !todo.isDone} : todo)
+        )
+
+
+
+
+    }
+
+
+    const handleEdit = (e: React.FormEvent, id:number) => {
+
+        e.preventDefault();
+
+
+        setTodos(
+            todos.map((todo) => (todo.id === id ? {...todo,todo:editTodo}: todo))
+        )
+
+
+        setEdit(false)
+
+    };
+
+
+
+
     return(
         <form className="todos_single">
           <span className="todos_single_text">{todo.todo}</span>
@@ -30,7 +73,7 @@ const SingleTodo  = ({todo,todos,setTodos} :props) => {
             <span className="icon">
                 <AiFillDelete/>
             </span>
-            <span className="icon">
+            <span className="icon" onClick={() => handleDone(todo.id)}>
                 <MdDone/>
             </span>
 
